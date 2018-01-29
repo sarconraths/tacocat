ea97c9afac4138341714820516a2294aa2469a9a
Sarah <30857692+sarconraths@users.noreply.github.com>
2018-01-29 07:18:51 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+  <title>Tacocat IT-Serivces</title>
+  <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Play" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <script src="Scripts/jquer_latest_2.11_.min.js" type="text/javascript"></script>
+  <script type="text/javascript" src="script.js"></script>
+</head>
+
+<body>
+  <?php include_once 'header.php' ?>
+  <?php include_once 'nav.php' ?>
+
+</body>
+</html
---
+++ nav.php
@@ -0,0 +1,26 @@
+<div class="top_nav">
+
+  <a href="support.php" class="nosign">
+    <div class="nav_card" id="service">
+     <span>IT-Support</span>
+    </div>
+  </a>
+
+  <a href="security.php" class="nosign">
+  <div class="nav_card">
+    <span>IT-Sicherheit</span>
+  </div>
+</a>
+  <a href="infra.php" class="nosign">
+  <div class="nav_card">
+    <span>IT-Infrastruktur</span>
+  </div>
+  </a>
+
+<a href="cloud.php" class="nosign">
+  <div class="nav_card">
+    <span>Cloud-Lösungen</span>
+  </div>
+  </a>
+
+</div>
