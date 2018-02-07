67d0f678d4e04a4f61e221ad6d4c59663e83455d
Sarah Conraths <schs@alpha.local>
2018-02-07 17:27:48 +0100

    New version 2018-02-07

--- index.php
+++ index.php
@@ -1,16 +1,78 @@
 <html>
 <head>
-  <title>Tacocat IT-Serivces</title>
+  <title>Tacocat IT-Serivce</title>
   <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Play" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="style.css">
-  <script src="Scripts/jquer_latest_2.11_.min.js" type="text/javascript"></script>
+  <script src="https://code.jquery.com/jquery-latest.js"></script>
   <script type="text/javascript" src="script.js"></script>
 </head>
 
 <body>
-  <?php include_once 'header.php' ?>
-  <?php include_once 'nav.php' ?>
+
+  <div class="top container">
+    <img src="http://pixelartmaker.com/art/445b4b928551ad8.png" height="100" />
+    <div class="top_text">
+      <span class="top_text-heading">Tacocat IT-Service</span><br />
+      <span>...gut beraten, wie man's auch dreht und wendet</span>
+    </div>
+  </div>
+
+  <div class="topnav">
+    <ul>
+      <a href="/leistungen">
+        <li class="menu-item">
+          Leistungen
+        </li>
+      </a>
+
+      <a href="/news">
+        <li class="menu-item">
+          News
+        </li>
+      </a>
+
+        <a href="/philosophie">
+          <li class="menu-item">
+            Unsere Philosophie
+          </li>
+      </a>
+
+      <a href="/karriere">
+        <li class="menu-item">
+          Karriere
+        </li>
+      </a>
+
+      <a href="/kontakt">
+        <li class="menu-item">
+          Kontakt
+        </li>
+      </a>
+    </ul>
+  </div>
+
+  <div class="container center content">
+    <h1>Ihr IT-Dienstleister im Herzen von Augsburg!</h1>
+
+
+    <div class="slider_container">
+      <div class="slider-nav slider-nav_left">
+        <i class="fa fa-angle-left"></i>
+      </div>
+
+
+      <div class="slider-nav slider-nav_right">
+        <i class="fa fa-angle-right"></i>
+      </div>
+
+      <div class="slide">
+        <img src="images/cloud.jpg" />
+      </div>
+
+    </div>
+  </div>
+
 
 </body>
 </html
